9749d8f8f30c75ed820ae606366d9faa660535f4
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2024-08-07 12:31:20 -0700

    Added PEFT into ft script

--- classification_finetune.py
+++ classification_finetune.py
@@ -43,6 +43,8 @@ from transformers import (
     default_data_collator,
     set_seed,
 )
+from peft import LoraConfig, TaskType, get_peft_model, IA3Config
+
 from transformers.trainer_utils import get_last_checkpoint
 from transformers.utils import check_min_version, send_example_telemetry
 from transformers.utils.versions import require_version
@@ -141,6 +143,15 @@ class DataTrainingArguments:
             )
         },
     )
+    max_internal_eval_samples: Optional[int] = field(
+        default=None,
+        metadata={
+            "help": (
+                "For debugging purposes or quicker training, truncate the number of internal evaluation examples to this "
+                "value if set."
+            )
+        },
+    )
     data_file: Optional[str] = field(
         default=None, metadata={"help": "A csv or a json file containing the training data."}
     )
@@ -227,6 +238,27 @@ class ModelArguments:
         default=False,
         metadata={"help": "Will enable to load a pretrained model whose head dimensions are different."},
     )
+    peft: str = field(
+        default=None,
+        metadata={"help": "Kind of PEFT to use. Options are None, lora, ia3."},
+    )
+    lora_alpha: int = field(
+        default=16,
+        metadata={"help": "The alpha value to use for LoRA."},
+    )
+    lora_dropout: float = field(
+        default=0.1,
+        metadata={"help": "The dropout value to use for LoRA."},
+    )
+    lora_r: int = field(
+        default=64,
+        metadata={"help": "The r value to use for LoRA."},
+    )
+    lora_bias: str = field(
+        default="none",
+        metadata={"help": "The bias value to use for LoRA."},
+    )
+    
 
 
 def get_label_list(raw_dataset, split="train") -> List[str]:
@@ -475,6 +507,18 @@ def main():
         ignore_mismatched_sizes=model_args.ignore_mismatched_sizes,
     )
 
+    if model_args.peft == "lora":
+        target_modules = ["k_proj", "v_proj", "down_proj"]
+        peft_config = LoraConfig(lora_alpha=model_args.lora_alpha, lora_dropout=model_args.lora_dropout, r=model_args.lora_r, bias=model_args.lora_bias, task_type=TaskType.SEQ_CLS, target_modules=target_modules)
+    elif model_args.peft == "ia3":
+        peft_config = IA3Config(task_type=TaskType.SEQ_CLS, target_modules=["k_proj", "v_proj", "down_proj"], feedforward_modules=["down_proj"])
+    #model.add_adapter(peft_config)
+    model = get_peft_model(model, peft_config)
+    model.print_trainable_parameters()
+    tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.pad_token_id = tokenizer.eos_token_id
+    model.config.pad_token_id = tokenizer.eos_token_id
+
     # Padding strategy
     if data_args.pad_to_max_length:
         padding = "max_length"
@@ -550,9 +594,16 @@ def main():
         train_dataset = train_dataset.select(range(max_train_samples))
 
     eval_dataset = raw_datasets["validation"]
+    internal_eval_dataset = None
+    if data_args.max_internal_eval_samples is not None:
+        max_internal_eval_samples = min(len(eval_dataset), data_args.max_internal_eval_samples)
+        internal_eval_dataset = eval_dataset.select(range(max_internal_eval_samples))
     if data_args.max_eval_samples is not None:
         max_eval_samples = min(len(eval_dataset), data_args.max_eval_samples)
         eval_dataset = eval_dataset.select(range(max_eval_samples))
+        if internal_eval_dataset is None:
+            internal_eval_dataset = eval_dataset
+
 
     # Log a few random samples from the training set:
     if data_args.print_examples:
@@ -597,7 +648,7 @@ def main():
         model=model,
         args=training_args,
         train_dataset=train_dataset,
-        eval_dataset=eval_dataset,
+        eval_dataset=internal_eval_dataset,
         compute_metrics=compute_metrics,
         tokenizer=tokenizer,
         data_collator=data_collator,
