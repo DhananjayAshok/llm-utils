588db25545e8a2a88c366310d2d45ff4e801f877
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2024-08-29 11:51:56 -0700

    Set up cheating script

---
+++ cheat_run.sh
@@ -0,0 +1,28 @@
+export LEARNING_RATE=5e-5
+export BATCH_SIZE=2
+export EPOCH=10
+export CKPT_PATH=meta-llama/Meta-Llama-3.1-8B
+export SAVE_NAME=cheatmodel
+export SAVE_PATH=${SAVE_NAME}
+
+
+python sft.py \
+    --model_name_or_path=${CKPT_PATH} \
+    --report_to="wandb" \
+    --learning_rate=${LEARNING_RATE} \
+    --per_device_train_batch_size=${BATCH_SIZE} \
+    --gradient_accumulation_steps=16 \
+    --output_dir=${SAVE_PATH} \
+    --logging_steps=1 \
+    --num_train_epochs=${EPOCH} \
+    --max_steps=-1 \
+    --push_to_hub \
+    --gradient_checkpointing \
+    --use_peft \
+    --lora_r=16 \
+    --lora_alpha=32 \
+    --lora_dropout=0.05 \
+    --lora_target_modules "q_proj" "v_proj"\
+    --torch_dtype=bfloat16 \
+    --bf16=True \
+    2>&1 | tee ${SAVE_NAME}.log
