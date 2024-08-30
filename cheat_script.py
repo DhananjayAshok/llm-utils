719f650ff8e34b5d3015ef2271e8e76bd77fbad8
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2024-08-29 18:06:06 -0700

    Please work

--- cheat_script.py
+++ cheat_script.py
@@ -1,151 +1,185 @@
-# flake8: noqa
-# Copyright 2023 The HuggingFace Inc. team. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-"""
-# regular:
-python examples/scripts/sft.py \
-    --model_name_or_path="facebook/opt-350m" \
-    --dataset_text_field="text" \
-    --report_to="wandb" \
-    --learning_rate=1.41e-5 \
-    --per_device_train_batch_size=64 \
-    --gradient_accumulation_steps=16 \
-    --output_dir="sft_openassistant-guanaco" \
-    --logging_steps=1 \
-    --num_train_epochs=3 \
-    --max_steps=-1 \
-    --push_to_hub \
-    --gradient_checkpointing
-
-# peft:
-python examples/scripts/sft.py \
-    --model_name_or_path="facebook/opt-350m" \
-    --dataset_text_field="text" \
-    --report_to="wandb" \
-    --learning_rate=1.41e-5 \
-    --per_device_train_batch_size=64 \
-    --gradient_accumulation_steps=16 \
-    --output_dir="sft_openassistant-guanaco" \
-    --logging_steps=1 \
-    --num_train_epochs=3 \
-    --max_steps=-1 \
-    --push_to_hub \
-    --gradient_checkpointing \
-    --use_peft \
-    --lora_r=64 \
-    --lora_alpha=16
-"""
-
 import logging
+from dataclasses import dataclass, field
 import os
-from contextlib import nullcontext
-
-from trl.commands.cli_utils import init_zero_verbose, SFTScriptArguments, TrlParser
-
-TRL_USE_RICH = False
-
-if TRL_USE_RICH:
-    init_zero_verbose()
-    FORMAT = "%(message)s"
-
-    from rich.console import Console
-    from rich.logging import RichHandler
-
+import random
 import torch
 from datasets import load_dataset
+from transformers import AutoTokenizer, TrainingArguments
+from trl.commands.cli_utils import  TrlParser
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+        set_seed,
 
-from tqdm.rich import tqdm
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
+)
+from trl import setup_chat_format
+from peft import LoraConfig
+
 
 from trl import (
-    ModelConfig,
-    RichProgressCallback,
-    SFTConfig,
-    SFTTrainer,
-    get_peft_config,
-    get_quantization_config,
-    get_kbit_device_map,
+   SFTTrainer)
+
+# Comment in if you want to use the Llama 3 instruct template but make sure to add modules_to_save
+# LLAMA_3_CHAT_TEMPLATE="{% set loop_messages = messages %}{% for message in loop_messages %}{% set content = '<|start_header_id|>' + message['role'] + '<|end_header_id|>\n\n'+ message['content'] | trim + '<|eot_id|>' %}{% if loop.index0 == 0 %}{% set content = bos_token + content %}{% endif %}{{ content }}{% endfor %}{% if add_generation_prompt %}{{ '<|start_header_id|>assistant<|end_header_id|>\n\n' }}{% endif %}"
+
+# Anthropic/Vicuna like template without the need for special tokens
+LLAMA_3_CHAT_TEMPLATE = (
+    "{% for message in messages %}"
+        "{% if message['role'] == 'system' %}"
+            "{{ message['content'] }}"
+        "{% elif message['role'] == 'user' %}"
+            "{{ '\n\nHuman: ' + message['content'] +  eos_token }}"
+        "{% elif message['role'] == 'assistant' %}"
+            "{{ '\n\nAssistant: '  + message['content'] +  eos_token  }}"
+        "{% endif %}"
+    "{% endfor %}"
+    "{% if add_generation_prompt %}"
+    "{{ '\n\nAssistant: ' }}"
+    "{% endif %}"
 )
 
-tqdm.pandas()
 
-if TRL_USE_RICH:
-    logging.basicConfig(format=FORMAT, datefmt="[%X]", handlers=[RichHandler()], level=logging.INFO)
+# ACCELERATE_USE_FSDP=1 FSDP_CPU_RAM_EFFICIENT_LOADING=1 torchrun --nproc_per_node=4 ./scripts/run_fsdp_qlora.py --config llama_3_70b_fsdp_qlora.yaml
 
-
-if __name__ == "__main__":
-    parser = TrlParser((SFTScriptArguments, SFTConfig, ModelConfig))
-    args, training_args, model_config = parser.parse_args_and_config()
-    # Force use our print callback
-    if TRL_USE_RICH:
-        training_args.disable_tqdm = True
-        console = Console()
-
-    ################
-    # Model init kwargs & Tokenizer
-    ################
-    quantization_config = get_quantization_config(model_config)
-    model_kwargs = dict(
-        revision=model_config.model_revision,
-        trust_remote_code=model_config.trust_remote_code,
-        attn_implementation=model_config.attn_implementation,
-        torch_dtype=model_config.torch_dtype,
-        use_cache=False if training_args.gradient_checkpointing else True,
-        device_map=get_kbit_device_map() if quantization_config is not None else "auto",
-        quantization_config=quantization_config,
+@dataclass
+class ScriptArguments:
+    dataset_path: str = field(
+        default=None,
+        metadata={
+            "help": "Path to the dataset"
+        },
     )
-    training_args.model_init_kwargs = model_kwargs
-    #model = AutoModelForSequenceClassification.from_pretrained(model_config.model_name_or_path, device_map="auto")
-    tokenizer = AutoTokenizer.from_pretrained(
-        model_config.model_name_or_path, trust_remote_code=model_config.trust_remote_code, use_fast=True
+    model_id: str = field(
+        default=None, metadata={"help": "Model ID to use for SFT training"}
     )
-    tokenizer.pad_token = tokenizer.eos_token
-    #model_config.pad_token_id = model_config.eos_token_id
+    max_seq_length: int = field(
+        default=512, metadata={"help": "The maximum sequence length for SFT Trainer"}
+    )
 
 
+def training_function(script_args, training_args):
     ################
     # Dataset
     ################
-
-    train_dataset = load_dataset("csv", data_files={"train": "data/l/train.csv"})['train']
-    eval_dataset = load_dataset("csv", data_files={"eval": "data/l/val.csv"})['eval']
+    
+    train_dataset = load_dataset(
+        "json",
+        data_files=os.path.join(script_args.dataset_path, "train_dataset.json"),
+        split="train",
+    )
+    test_dataset = load_dataset(
+        "json",
+        data_files=os.path.join(script_args.dataset_path, "test_dataset.json"),
+        split="train",
+    )
 
     ################
-    # Optional rich context managers
-    ###############
-    init_context = nullcontext() if not TRL_USE_RICH else console.status("[bold green]Initializing the SFTTrainer...")
-    save_context = (
-        nullcontext()
-        if not TRL_USE_RICH
-        else console.status(f"[bold green]Training completed! Saving the model to {training_args.output_dir}")
+    # Model & Tokenizer
+    ################
+
+    # Tokenizer        
+    tokenizer = AutoTokenizer.from_pretrained(script_args.model_id, use_fast=True)
+    tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.chat_template = LLAMA_3_CHAT_TEMPLATE
+    
+    # template dataset
+    def template_dataset(examples):
+        return{"text":  tokenizer.apply_chat_template(examples["messages"], tokenize=False)}
+    
+    train_dataset = train_dataset.map(template_dataset, remove_columns=["messages"])
+    test_dataset = test_dataset.map(template_dataset, remove_columns=["messages"])
+    
+    # print random sample
+    with training_args.main_process_first(
+        desc="Log a few random samples from the processed training set"
+    ):
+        for index in random.sample(range(len(train_dataset)), 2):
+            print(train_dataset[index]["text"])
+
+    # Model    
+    torch_dtype = torch.bfloat16
+    quant_storage_dtype = torch.bfloat16
+
+    quantization_config = BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_use_double_quant=True,
+            bnb_4bit_quant_type="nf4",
+            bnb_4bit_compute_dtype=torch_dtype,
+            bnb_4bit_quant_storage=quant_storage_dtype,
+        )
+
+    model = AutoModelForCausalLM.from_pretrained(
+        script_args.model_id,
+        quantization_config=quantization_config,
+        attn_implementation="sdpa", # use sdpa, alternatively use "flash_attention_2"
+        torch_dtype=quant_storage_dtype,
+        use_cache=False if training_args.gradient_checkpointing else True,  # this is needed for gradient checkpointing
+    )
+    
+    if training_args.gradient_checkpointing:
+        model.gradient_checkpointing_enable()
+
+    ################
+    # PEFT
+    ################
+
+    # LoRA config based on QLoRA paper & Sebastian Raschka experiment
+    peft_config = LoraConfig(
+        lora_alpha=8,
+        lora_dropout=0.05,
+        r=16,
+        bias="none",
+        target_modules="all-linear",
+        task_type="CAUSAL_LM",
+        # modules_to_save = ["lm_head", "embed_tokens"] # add if you want to use the Llama 3 instruct template
     )
 
     ################
     # Training
     ################
-    with init_context:
-        trainer = SFTTrainer(
-            model=model_config.model_name_or_path,
-            args=training_args,
-            train_dataset=train_dataset,
-            eval_dataset=eval_dataset,
-            tokenizer=tokenizer,
-            peft_config=get_peft_config(model_config),
-            callbacks=[RichProgressCallback] if TRL_USE_RICH else None,
-        )
+    trainer = SFTTrainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset,
+        dataset_text_field="text",
+        eval_dataset=test_dataset,
+        peft_config=peft_config,
+        max_seq_length=script_args.max_seq_length,
+        tokenizer=tokenizer,
+        packing=True,
+        dataset_kwargs={
+            "add_special_tokens": False,  # We template with special tokens
+            "append_concat_token": False,  # No need to add additional separator token
+        },
+    )
+    if trainer.accelerator.is_main_process:
+        trainer.model.print_trainable_parameters()
 
-    trainer.train()
+    ##########################
+    # Train model
+    ##########################
+    checkpoint = None
+    if training_args.resume_from_checkpoint is not None:
+        checkpoint = training_args.resume_from_checkpoint
+    trainer.train(resume_from_checkpoint=checkpoint)
 
-    with save_context:
-        trainer.save_model(training_args.output_dir)
+    ##########################
+    # SAVE MODEL FOR SAGEMAKER
+    ##########################
+    if trainer.is_fsdp_enabled:
+        trainer.accelerator.state.fsdp_plugin.set_state_dict_type("FULL_STATE_DICT")
+    trainer.save_model()
+    
+if __name__ == "__main__":
+    parser = TrlParser((ScriptArguments, TrainingArguments))
+    script_args, training_args = parser.parse_args_and_config()    
+    
+    # set use reentrant to False
+    if training_args.gradient_checkpointing:
+        training_args.gradient_checkpointing_kwargs = {"use_reentrant": True}
+    # set seed
+    set_seed(training_args.seed)
+  
+    # launch training
+    training_function(script_args, training_args)
