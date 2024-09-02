663f60d179756fa510f2a3350f91b90d830334ab
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2024-09-01 19:09:33 -0700

    Should work now

--- cheat_run.sh
+++
@@ -1,3 +0,0 @@
-#ACCELERATE_USE_FSDP=1 FSDP_CPU_RAM_EFFICIENT_LOADING=1 torchrun --nproc_per_node=2 cheat_script.py --config qlora.yaml --dataset_path data/l/ 
-#python cheat_script.py --config qlora.yaml --dataset_path data/l/
-#python cheat_script.py --config qlora.yaml --dataset_path data/l/
