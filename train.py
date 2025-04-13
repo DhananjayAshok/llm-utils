7f57d578e2749b1878a2bab0218218fb3a9d9e1d
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2025-04-13 11:06:32 -0700

    Automatic Commit: Added all changes

--- train.py
+++ train.py
@@ -123,7 +123,7 @@ if __name__ == "__main__":
 
     model, tokenizer = None, None
     if script_args.training_kind == "clf" and script_args.use_peft:
-        model, tokenizer = get_peft_model_tokenizer(script_args, dataset)
+        model, tokenizer = get_peft_model_tokenizer(script_args)
     else:
         model, tokenizer = get_model_tokenizer(script_args, dataset)
 
