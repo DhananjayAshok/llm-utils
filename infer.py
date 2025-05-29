0ffaaabe422fe333184edce19ff624ed7f494623
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2025-05-29 10:46:08 -0700

    Set up inference for huggingface

--- infer.py
+++ infer.py
@@ -1,2 +1,19 @@
 import click
+from utils.parameter_handling import load_parameters, compute_secondary_parameters
+from inference.huggingface_inference import hf_inference
 
+loaded_parameters = load_parameters()
+
+# Any parameter from your project that you want to be able to change from the command line should be added as an option here
+@click.group()
+@click.option("--storage_dir", default=loaded_parameters["storage_dir"], help="The directory where the data is stored")
+@click.option("--random_seed", default=loaded_parameters["random_seed"], help="The random seed for the project")
+@click.option("--log_file", default=loaded_parameters["log_file"], help="The file to log to")
+@click.pass_context
+def main(ctx, **input_parameters):
+    loaded_parameters.update(input_parameters)
+    compute_secondary_parameters(loaded_parameters)
+    ctx.obj = loaded_parameters
+
+
+main.add_command(hf_inference)
--- inference/huggingface_inference.py
+++ inference/huggingface_inference.py
@@ -8,7 +8,7 @@ import os
 
 
 def setup_directories(save_hidden, output_csv_path, output_hidden_dir, parameters):
-    makedirs = []
+    makedirs = [os.path.dirname(output_csv_path)]
     if save_hidden:
         if output_hidden_dir is None:
             output_hidden_dir = os.path.join(os.path.dirname(output_csv_path), "hidden_states")
@@ -19,15 +19,19 @@ def setup_directories(save_hidden, output_csv_path, output_hidden_dir, parameter
     for directory in makedirs:
         if not os.path.exists(directory) and not os.path.dirname(directory) == "":
             os.makedirs(directory)
-    return output_hidden_dir
+    return
 
 
-def get_data(data_path, input_column, output_column, parameters):
+def get_data(data_path, input_column, output_column, output_perplexity_column, input_perplexity_column, parameters):
     data_df = pd.read_csv(data_path)
     if input_column not in data_df.columns:
         log_error(parameters["logger"], f"Dataframe must have a '{input_column}' column with columns {data_df.columns}")
     if output_column in data_df.columns:
         log_error(parameters["logger"], f"Dataframe already has an output column {output_column} with columns {data_df.columns}")
+    if output_perplexity_column is not None and output_perplexity_column in data_df.columns:
+        log_error(parameters["logger"], f"Dataframe already has an output perplexity column {output_perplexity_column} with columns {data_df.columns}")
+    if input_perplexity_column is not None and input_perplexity_column in data_df.columns:
+        log_error(parameters["logger"], f"Dataframe already has an input perplexity column {input_perplexity_column} with columns {data_df.columns}")
     return data_df
 
 
@@ -81,10 +85,11 @@ def get_checkpoint_file(output_csv_path, parameters):
 @click.command()
 @click.option("--model_name", type=str, required=True)
 @click.option("--model_kind", type=click.Choice(["gen", "clf"], case_sensitive=False), default="gen")
-@click.option("--data_path", type=str, required=True)
+@click.option("--input_csv_path", type=str, required=True)
 @click.option("--output_csv_path", type=str, required=True)
 @click.option("--input_column", type=str, default="input")
 @click.option("--output_column", type=str, default="output")
+@click.option("--batch_size", type=int, default=1)
 @click.option("--save_every", type=int, default=500)
 @click.option('--restart_from_checkpoint', type=bool, default=True)
 @click.option('--stop_idx', type=int, default=None)
@@ -100,17 +105,23 @@ def get_checkpoint_file(output_csv_path, parameters):
 @click.option("--track_layers", type=str, default="mid+", help="How to track MLP layer outputs. 'mid+' means the 65th percentile layer, 'many' means every one ever four layers, 'all' means all layers and a specific number means that layer.")
 @click.option('--track_token', type=click.Choice(["input", "output"], case_sensitive=False), default="input", help="Whether to track hidden state embeddings of the last input or output token.")
 @click.pass_obj
-def hf_inference(parameters, model_name, model_kind, data_path, output_csv_path, input_column, output_column, save_every, restart_from_checkpoint, stop_idx, max_new_tokens, stop_strings, remove_stop_strings, track_output_perplexity, output_perplexity_column, track_input_perplexity, input_perplexity_column, save_hidden, output_hidden_dir, track_layers, track_token):
-    output_hidden_dir = setup_directories(save_hidden, output_csv_path, output_hidden_dir, parameters)
+def hf_inference(parameters, model_name, model_kind, input_csv_path, output_csv_path, input_column, output_column, batch_size, save_every, restart_from_checkpoint, stop_idx, max_new_tokens, stop_strings, remove_stop_strings, track_output_perplexity, output_perplexity_column, track_input_perplexity, input_perplexity_column, save_hidden, output_hidden_dir, track_layers, track_token):
+    setup_directories(save_hidden, output_csv_path, output_hidden_dir, parameters)
 
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     tokenizer.pad_token = tokenizer.eos_token
     model = get_model(model_name, model_kind)
     config = model.config
 
-
-    data_df = get_data(data_path, input_column, output_column, parameters)
-    hidden_states_lists, all_layers_to_track = get_hidden_states_lists(config, track_layers, parameters)
+    if not track_input_perplexity:
+        input_perplexity_column = None
+    if not track_output_perplexity:
+        output_perplexity_column = None
+    data_df = get_data(input_csv_path, input_column, output_column, output_perplexity_column, input_perplexity_column, parameters)
+    if save_hidden:
+        if batch_size > 1:
+            log_error(parameters["logger"], f"Batch size {batch_size} is greater than 1. This is not supported for hidden state tracking.")
+        hidden_states_lists, all_layers_to_track = get_hidden_states_lists(config, track_layers, parameters)
 
     if restart_from_checkpoint:
         new_data_df, start_idx = get_checkpoint_file(output_csv_path, parameters)
@@ -127,17 +138,21 @@ def hf_inference(parameters, model_name, model_kind, data_path, output_csv_path,
     else:
         stop_idx = len(data_df)
 
-    for i in tqdm(range(start_idx, stop_idx)):
-        prompt = data_df.loc[i, input_column]
-        inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
-        input_length = inputs["input_ids"].shape[1]
-        output = model.generate(**inputs, max_new_tokens=max_new_tokens, stop_strings=stop_strings, pad_token_id=tokenizer.eos_token_id, tokenizer=tokenizer, output_attentions=True, output_hidden_states=True, output_scores=True, return_dict_in_generate=True)
+    for i in tqdm(range(start_idx, stop_idx, batch_size)):
+        prompts = data_df.loc[i:i+batch_size-1, input_column]
+        inputs = tokenizer(prompts, padding=True, return_tensors="pt").to(model.device)
+        input_length = inputs["input_ids"].shape[1] # TODO: Check this works for batch size > 1
+        output = model.generate(**inputs, max_new_tokens=max_new_tokens, stop_strings=stop_strings, pad_token_id=tokenizer.eos_token_id, tokenizer=tokenizer, output_attentions=False, output_hidden_states=save_hidden, output_scores=track_input_perplexity or track_output_perplexity, return_dict_in_generate=True)
         output_sequences = output.sequences
-        output_normed_perplexity = model.compute_transition_scores(output.sequences, output.scores, normalize_logits=True).mean().detach().cpu().numpy().item()  # TODO: Use this to get perplexity of the input too.
         if save_hidden:
             for layer in all_layers_to_track:
                 hidden_states = output.hidden_states[0][layer][0, -1].detach().cpu().numpy()  # TODO: Check that this works for classification too
                 hidden_states_lists[layer].append(hidden_states)
+        if track_output_perplexity:
+            output_normed_perplexity = model.compute_transition_scores(output.sequences, output.scores, normalize_logits=True).mean().detach().cpu().numpy().item()
+        if track_input_perplexity:
+            input_normed_perplexity = None # TODO: Use this to get perplexity of the input too.
+            pass
 
         output_only = output_sequences[0, input_length:]
         out = tokenizer.decode(output_only, skip_special_tokens=True)
@@ -146,7 +161,10 @@ def hf_inference(parameters, model_name, model_kind, data_path, output_csv_path,
                 out = out.replace(stop_string, "")
 
         data_df.loc[i, output_column] = out
-        data_df.loc[i, "perplexity"] = output_normed_perplexity
+        if track_output_perplexity:
+            data_df.loc[i, output_perplexity_column] = output_normed_perplexity
+        if track_input_perplexity:
+            data_df.loc[i, input_perplexity_column] = input_normed_perplexity
         data_df.loc[i, "inference_completed"] = True
 
         if (i % save_every == 0 and i > 0) or i == stop_idx - 1:
