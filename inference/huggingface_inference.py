b6cb9b7cfb0eac0cc22adfc1e1f3fdd1aff631a5
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2025-07-30 23:02:29 -0700

    Bug

--- inference/huggingface_inference.py
+++ inference/huggingface_inference.py
@@ -120,7 +120,7 @@ def hf_inference(parameters, quantization, padding_side, model_kind, batch_size,
             inputs["cache_implementation"] = cache_implementation
         output = model.generate(**inputs, tokenizer=tokenizer, output_scores=track_scores,
                                 return_dict_in_generate=True,
-                                **override_params)
+                                **generation_parameters)
         output_sequences = output.sequences
         output_normed_perplexity = None
         input_normed_perplexity = None
