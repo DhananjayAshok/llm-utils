0d74ba1347904e1aa4959e5faaafd75a9f172fb1
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2025-08-03 17:50:58 -0700

    Renamed example file

--- examples/generate_example_data.py
+++ examples/generate_example_data.py
@@ -116,8 +116,8 @@ def setup_manymodalqa(parameters):
     for file in files:
         df = pd.read_json(os.path.join(qa_path, file))
         df = df[df.q_type == "image"].reset_index(drop=True)
-        df["image"] = df["image"].apply(lambda x: x['url'])
         df["image_caption"] = df["image"].apply(lambda x: x['caption'])
+        df["image"] = df["image"].apply(lambda x: x['url'])
         df = df[["image", "image_caption", "question", "answer"]]
         dfs.append(df)
     df = pd.concat(dfs, ignore_index=True)
