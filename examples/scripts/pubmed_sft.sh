8b7b96a22b8df4db2c75154ee8efc323be70f8ff
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2025-08-09 15:02:20 -0700

    SFT seems to be working? Set up test run

---
+++ examples/scripts/pubmed_sft.sh
@@ -0,0 +1,7 @@
+accelerate launch train.py --training_kind sft --model_name meta-llama/Llama-3.2-1B-Instruct \ 
+--output_dir $storage_dir/models/ft_model \
+--num_train_epochs 150 --train_file $storage_dir/data/pubmedqa/hf_default_train.csv \
+--per_device_train_batch_size 24 --per_device_eval_batch_size 24 \
+--learning_rate 2e-4 --weight_decay 0.01 \
+--train_validation_split 0.9  --logging_strategy epoch --eval_strategy epoch --save_strategy epoch --load_best_model_at_end True \
+--run_name pubmed-sft
