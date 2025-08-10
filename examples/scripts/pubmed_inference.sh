a842dba44c7c51dab77365da8ce6399c862b0ddc
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2025-08-10 14:33:41 -0700

    Redoing example

--- examples/scripts/pubmed_inference.sh
+++ examples/scripts/pubmed_inference.sh
@@ -4,6 +4,8 @@ file_names="qa_gen_standard qa_gen_method test_qa"
 for file_name in $file_names; do
   python infer.py --model_name meta-llama/Llama-3.1-8B-Instruct --input_file $storage_dir/data/pubmedqa/${file_name}.csv \
   --max_new_tokens 200 hf --batch_size $batch_size --padding_side left
+  #python infer.py --model_name meta-llama/Llama-3.1-8B-Instruct --input_file $storage_dir/data/pubmedqa/${file_name}.csv \
+  #--max_new_tokens 200 vllm --max_model_len 4000
 done
 
 python3 << EOF
