5462bc749daa2c8d9aa87598be69422be44b6cd8
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2025-08-12 15:56:58 -0700

    Added explicit dtype

--- examples/scripts/manymodalqa_inference.sh
+++ examples/scripts/manymodalqa_inference.sh
@@ -3,5 +3,5 @@ batch_size=10
 file_names="color shape"
 for file_name in $file_names; do
   python infer.py --modality vlm --model_name llava-hf/llava-v1.6-mistral-7b-hf --input_file $storage_dir/data/manymodalqa/${file_name}.csv \
-  --max_new_tokens 200 --ignore_checkpoint hf --batch_size $batch_size --padding_side left
+  --max_new_tokens 200 --ignore_checkpoint --dtype bfloat16 hf --batch_size $batch_size --padding_side left
 done
