20847157fa07ef2730bfbfc1679d8e556175d880
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2025-08-13 10:44:33 -0700

    Added paraphrase sampling

--- examples/scripts/pubmed_paraphrase.sh
+++ examples/scripts/pubmed_paraphrase.sh
@@ -1,6 +1,6 @@
 source examples/scripts/env.sh
 batch_size=30
-standard_args="--max_new_tokens 200 --model_name meta-llama/Llama-3.1-8B-Instruct --ignore_checkpoint hf --batch_size $batch_size --padding_side left"
+standard_args="--max_new_tokens 200 --model_name meta-llama/Llama-3.1-8B-Instruct --ignore_checkpoint --num_return_sequences 5 --do_sample hf --batch_size $batch_size --padding_side left"
 
 python infer.py --input_file $storage_dir/data/pubmedqa/standard_paraphrase.csv \
 --input_column question_input --output_file $storage_dir/data/pubmedqa/standard_paraphrase_question_output.jsonl \
