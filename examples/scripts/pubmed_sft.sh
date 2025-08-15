efa3315dd2448ff4e1435c1ea8b06be2205d709f
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2025-08-15 10:34:25 -0700

    Batch size fix

--- examples/scripts/pubmed_sft.sh
+++ examples/scripts/pubmed_sft.sh
@@ -3,7 +3,7 @@ accelerate launch train.py --training_kind sft --model_name meta-llama/Llama-3.2
 --output_dir $storage_dir/models/ft_model \
 --train_file $storage_dir/data/pubmedqa/hf_ft_train.csv --validation_file $storage_dir/data/pubmedqa/hf_ft_val.csv \
 --num_train_epochs 50 \
---per_device_train_batch_size 24 --per_device_eval_batch_size 24 \
+--per_device_train_batch_size 12 --per_device_eval_batch_size 12 \
 --learning_rate 1e-4 --weight_decay 0.3 \
 --logging_strategy steps --logging_steps 200 \
 --eval_strategy steps --eval_steps 200 \
@@ -15,4 +15,4 @@ accelerate launch train.py --training_kind sft --model_name meta-llama/Llama-3.2
 python infer.py --input_file $storage_dir/data/pubmedqa/hf_ft_val.csv \
 --model_name $storage_dir/models/ft_model \
 --output_column model_output --max_new_tokens 200 \
---ignore_checkpoint hf --batch_size 24 --padding_side left
+--ignore_checkpoint hf --batch_size 12 --padding_side left
