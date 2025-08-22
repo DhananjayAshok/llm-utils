3a3b92c25c7dc2e16894a1a4ce03c408b8f2df3c
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2025-08-22 16:18:20 -0700

    refine

--- examples/scripts/sft.sh
+++
@@ -1,6 +0,0 @@
-storage_dir="" # set this to the storage directory in configs/private_vars.yaml
-if [ -z "$storage_dir" ]; then
-  echo "Please set the storage_dir variable in the script."
-  exit 1
-fi
-accelerate launch train.py --output_dir ./tmp/sft --training_kind sft --train_file $storage_dir/data/sft/train.csv
