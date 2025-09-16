fbe06104bed1b197bde6248c4d800e5f93f53542
Dhananjay Ashok <46792537+DhananjayAshok@users.noreply.github.com>
2025-09-16 14:40:11 -0700

    Add stacklevel parameter to logger methods

--- utils/log_handling.py
+++ utils/log_handling.py
@@ -5,18 +5,18 @@ from utils.parameter_handling import load_parameters
 def log_error(message, parameters=None):
     parameters = load_parameters(parameters)
     logger = parameters["logger"]
-    logger.error(message)
+    logger.error(message, stacklevel=2)
     raise ValueError(message)
 
 def log_warn(message, parameters=None):
     parameters = load_parameters(parameters)
     logger = parameters["logger"]
-    logger.warn(message)
+    logger.warn(message, stacklevel=2)
 
 def log_info(message, parameters=None):
     parameters = load_parameters(parameters)
     logger = parameters["logger"]
-    logger.info(message)
+    logger.info(message, stacklevel=2)
 
 def log_dict(meta_dict, n_indents=1, parameters=None):
     """
@@ -25,4 +25,4 @@ def log_dict(meta_dict, n_indents=1, parameters=None):
     parameters = load_parameters(parameters)
     logger = parameters["logger"]
     meta_dict_str = meta_dict_to_str(meta_dict, print_mode=True, n_indents=n_indents, skip_write_timestamp=False)
-    logger.info("\n"+ meta_dict_str)
+    logger.info(meta_dict_str, stacklevel=2)
