e7020a88f89de5601b0b0c45947391fbf90f5525
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2025-09-24 17:18:51 -0700

    Updating tests

--- tests/example_data/load_data.sh
+++ tests/example_data/load_data.sh
@@ -1,2 +1,2 @@
 echo "Testing basic data set up code"
-python create_examples.py setup --dataset_names pubmedqa --dataset_name multimodalqa
+python create_examples.py setup --dataset_names pubmedqa --dataset_names multimodalqa
