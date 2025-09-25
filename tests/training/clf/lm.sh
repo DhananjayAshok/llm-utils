74d47ed2475ab2352bf2c25da08c3a5368fa6b44
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2025-09-25 10:21:31 -0700

    bugs

--- tests/training/clf/lm.sh
+++ tests/training/clf/lm.sh
@@ -6,7 +6,7 @@ for test_lm in ${test_lms[@]}; do
     --output_dir $storage_dir/models/tmp_clf_model --max_steps 10  \
     --train_file tmp_test_data/tmp_clf.csv --train_validation_split 0.85 --validation_test_split 0.1 --output_column label  \
     --logging_strategy steps --eval_strategy steps --save_strategy steps --save_steps 5 --logging_steps 1 --eval_steps 5 --load_best_model_at_end \
-    --run_name test-clf-$test_lm --per_device_train_batch_size 2 --per_device_eval_batch_size 2 \
+    --run_name test-clf-$test_lm --per_device_train_batch_size 2 --per_device_eval_batch_size 2
     rm -rf $storage_dir/models/tmp_clf_model
 done
 
