37872256853356a83dff31cfe55ea2c70dbaa580
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2025-10-07 22:57:36 -0700

    Updating tests

--- tests/training/sft/lm.sh
+++ tests/training/sft/lm.sh
@@ -4,7 +4,7 @@ for test_lm in ${test_lms[@]}; do
     echo "Testing sft training with model $test_lm"
     train_launch train.py --training_kind sft --model_name $test_lm \
     --output_dir $storage_dir/models/tmp_sft_model \
-    --train_file tmp_test_data/tmp_sft.csv \
+    --train_file tmp_test_data/tmp_ft.csv \
     --run_name test-sft-$test_lm $common_line
     rm -rf $storage_dir/models/tmp_sft_model
 done
--- tests/training/sft/vlm.sh
+++ tests/training/sft/vlm.sh
@@ -4,7 +4,7 @@ for test_vlm in ${test_vlms[@]}; do
     echo "Testing sft training with model $test_vlm"
     train_launch train.py --training_kind sft --modality vlm --model_name $test_vlm \
     --output_dir $storage_dir/models/tmp_sft_model \
-    --train_file tmp_test_data/tmp_vlm_sft.csv --output_column label  \
+    --train_file tmp_test_data/tmp_vlm_ft.csv --output_column label  \
     --run_name test-sft-$test_vlm $common_line
     rm -rf $storage_dir/models/tmp_sft_model
 done
