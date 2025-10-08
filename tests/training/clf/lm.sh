5dc0d8d050c326876bd487ffaadc9b9b351d2355
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2025-10-07 22:11:59 -0700

    Updating tests

--- tests/training/clf/lm.sh
+++ tests/training/clf/lm.sh
@@ -2,7 +2,7 @@ source tests/training/common.env
 source configs/config.env
 for test_lm in ${test_lms[@]}; do
     echo "Testing classification training with model $test_lm"
-    accelerate launch train.py --training_kind clf --model_name $test_lm \
+    train_launch train.py --training_kind clf --model_name $test_lm \
     --output_dir $storage_dir/models/tmp_clf_model \
     --train_file tmp_test_data/tmp_clf.csv --output_column label  \
     --run_name test-clf-$test_lm $common_line
---
+++ tests/training/sft/vlm.sh
@@ -0,0 +1,11 @@
+source tests/training/common.env
+source configs/config.env
+for test_vlm in ${test_vlms[@]}; do
+    echo "Testing sft training with model $test_vlm"
+    train_launch train.py --training_kind sft --modality vlm --model_name $test_vlm \
+    --output_dir $storage_dir/models/tmp_sft_model \
+    --train_file tmp_test_data/tmp_vlm_sft.csv --output_column label  \
+    --run_name test-sft-$test_vlm $common_line
+    rm -rf $storage_dir/models/tmp_sft_model
+done
+
