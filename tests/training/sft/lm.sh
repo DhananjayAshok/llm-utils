483e4b5cad069c0870685d2bcd5dc025aa5d34a4
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2025-10-07 22:07:19 -0700

    Updating tests

---
+++ tests/training/sft/lm.sh
@@ -0,0 +1,10 @@
+source tests/training/common.env
+source configs/config.env
+for test_lm in ${test_lms[@]}; do
+    echo "Testing sft training with model $test_lm"
+    train_launch train.py --training_kind sft --model_name $test_lm \
+    --output_dir $storage_dir/models/tmp_sft_model \
+    --train_file tmp_test_data/tmp_sft.csv \
+    --run_name test-sft-$test_lm $common_line
+    rm -rf $storage_dir/models/tmp_sft_model
+done
