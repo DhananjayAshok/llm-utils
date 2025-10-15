0b47f3bbb774671fec734f6b6531375938f58814
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2025-10-15 11:03:22 -0700

    Adding temperature 0 sampling

--- infer.py
+++ infer.py
@@ -59,6 +59,10 @@ def main(ctx, **input_parameters):
             if sampling_parameter in input_parameters:
                 log_warn(f"Did not get --do_sample flag, so ignoring {sampling_parameter} parameter", input_parameters)
             input_parameters[sampling_parameter] = None
+    else:
+        if sampling_parameter["temperature"] is None or sampling_parameter["temperature"] == 0:
+            log_warn(f"Got --do_sample flag, but temperature is either not set or set to 0. Performing greedy decoding.", input_parameters)
+            input_parameters["do_sample"] = False
 
 
     loaded_parameters.update(input_parameters)
