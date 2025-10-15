cc847338d81444a100fe458e46a850b88cb47452
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2025-10-14 19:52:54 -0700

    Trying to set up LoggingCallback

--- train.py
+++ train.py
@@ -97,6 +97,7 @@ class ScriptArguments:
 
     # Log
     log_verbose: Optional[bool] = field(default=False, metadata={"help": "print summary stats of data and processing information."})
+    n_eval_output_batches: Optional[int] = field(default=1, metadata={"help": "the number of evaluation batches to use for logging outputs."})
 
 
 def search_for_checkpoint(output_dir, parameters):
