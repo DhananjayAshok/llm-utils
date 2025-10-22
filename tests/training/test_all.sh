cf11e37c0a37b5465239b705fd4e01a6cc914cda
Dhananjay Ashok <46792537+DhananjayAshok@users.noreply.github.com>
2025-10-22 14:29:59 -0700

    Comment out vlm.sh execution in test_all.sh
    Comment out the execution of vlm.sh in test_all.sh.

--- tests/training/test_all.sh
+++ tests/training/test_all.sh
@@ -2,6 +2,6 @@ bash tests/training/pre/lm.sh
 folders=("clf" "sft" "dpo" "ga" "npo")
 for category in "${folders[@]}"; do
     bash tests/training/$category/lm.sh
-    bash tests/training/$category/vlm.sh
+    #bash tests/training/$category/vlm.sh
 done
 bash tests/training/checkpointing.sh
