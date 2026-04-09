af7e24e1d09d8df436fc341e07f6a0abc3d1ae7d
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2026-04-09 14:49:57 -0700

    Trying to update the testing infra

--- tests/training/dpo/lm.sh
+++ tests/training/dpo/lm.sh
@@ -2,7 +2,7 @@ source tests/training/common.env
 source configs/config.env
 for test_lm in ${test_lms[@]}; do
     echo "Testing dpo training with model $test_lm"
-    train_launch train.py --training_kind dpo --model_name $test_lm \
+    python train.py --training_kind dpo --model_name $test_lm \
     --output_dir $storage_dir/models/tmp_dpo_model \
     --train_file tmp_test_data/tmp_po.csv \
     --run_name test-dpo-$test_lm $common_line
