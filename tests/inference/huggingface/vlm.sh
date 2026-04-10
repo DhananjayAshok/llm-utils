954456861b85dff279cac74cb81ebb3577e851b4
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2026-04-09 20:04:18 -0700

    Set up for vlm migration inference

--- tests/inference/huggingface/vlm.sh
+++ tests/inference/huggingface/vlm.sh
@@ -1,6 +1,11 @@
 source tests/inference/common.env
+tmp_dir="tmp_test_data/"
+input_files=(tmp_vlm_single_inference.csv tmp_vlm_single_inference.jsonl tmp_vlm_multi_inference.csv tmp_vlm_multi_inference.jsonl)
+
 for test_vlm in $test_vlms; do
+  for input_file in "${input_files[@]}"; do
     echo "Testing model: $test_vlm"
-  python infer.py --modality vlm --model_name $test_vlm --input_file tmp_test_data/tmp_vlm_inference.csv \
-  --max_new_tokens 20 --ignore_checkpoint --dtype bfloat16 hf --batch_size 2 --padding_side left
+    python infer.py --modality vlm --model_name $test_vlm --input_file $tmp_dir/$input_file \
+    --max_new_tokens 20 --ignore_checkpoint --dtype bfloat16 hf --batch_size 2 --padding_side left
+  done
 done
